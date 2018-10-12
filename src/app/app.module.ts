e51c543eb503ab82030ceb3450d78dd3a5b764bf
Leocaliban <leocaliban@gmail.com>
2018-10-12 18:23:34 -0300

    Configura as rotas de navegação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
 
+const rotas: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent },
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'pessoas', component: PessoasPesquisaComponent },
+  { path: 'pessoas/novo', component: PessoaCadastroComponent }
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +30,8 @@ import { CoreModule } from './core/core.module';
     CoreModule,
     LancamentosModule,
     PessoasModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(rotas)
   ],
   providers: [],
   bootstrap: [AppComponent]
