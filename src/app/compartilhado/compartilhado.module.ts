b63640af32b6168c3245088d90d586198e428247
Leocaliban <leocaliban@gmail.com>
2019-02-21 02:19:56 -0300

    Fix ERROR: BrowserModule has already been loaded.

--- src/app/compartilhado/compartilhado.module.ts
+++ src/app/compartilhado/compartilhado.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MessageModule } from 'primeng/message';
 import { TableModule } from 'primeng/table';
@@ -15,7 +14,6 @@ import { MensagemComponent } from './mensagem/mensagem.component';
     MessageModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
     TableModule
   ],
   declarations: [MensagemComponent],
