4a058026d387bb20cc560717e7b74a2ec51f53ec
Leocaliban <leocaliban@gmail.com>
2019-03-01 05:14:25 -0300

    Cria o formulário de relatórios

--- src/app/relatorios/relatorios.module.ts
+++ src/app/relatorios/relatorios.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { RelatoriosRoutingModule } from './relatorios-routing.module';
-import { SharedModule } from 'primeng/components/common/shared';
 import { RelatorioLancamentosComponent } from './relatorio-lancamentos/relatorio-lancamentos.component';
+import { CompartilhadoModule } from '../compartilhado/compartilhado.module';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   imports: [
     CommonModule,
     RelatoriosRoutingModule,
-    SharedModule
+    CompartilhadoModule,
+    FormsModule,
+    CalendarModule
   ],
   declarations: [RelatorioLancamentosComponent]
 })
