8617d85227cd583ced8bc199a72a7c85e993f102
Leocaliban <leocaliban@gmail.com>
2019-03-06 16:35:20 -0300

    FIX para ng build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiURL: 'https://leocaliban-finance-api.herokuapp.com/',
 
-  tokenWhitelistedDomains: [/leocaliban-finance-api.herokuapp.com/],
-  tokenBlacklistedRoutes: [/\/oauth\/token/]
+  tokenWhitelistedDomains: [new RegExp('leocaliban-finance-api.herokuapp.com')],
+  tokenBlacklistedRoutes: [new RegExp('\/oauth\/token')]
 };
